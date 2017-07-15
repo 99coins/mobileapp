8805fb230a60a166c735025a8a4c50dc86d5e0d6
Matan Guttman <matang@sweetinn.com>
2017-07-15 17:05:15 +0300

    stories header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //create comonent
-const Header = () => {
-    return <Text>Albums!</Text>;
+const Header = (props) => {
+    const { textStyle, viewStyle, subtitleStyle } = styles;
+    return (
+        <View style={viewStyle}>
+             <Text style={textStyle} >{props.titleText}</Text>
+             <Text style={subtitleStyle} >{props.subtitleText}</Text>
+
+        </View>
+    );
+};
+
+//styling
+
+const styles = {
+    viewStyle: {
+        height: 86,
+        backgroundColor: '#B91121',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 20
+    },
+    textStyle: {
+        fontSize: 20,
+        color: '#FFFFFF'
+    },
+    subtitleStyle: {
+        fontSize: 16,
+        color: '#676977'
+    }
 };
 
 //export to other areas of the app
