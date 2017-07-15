803a1cf3a82cacb6aeb24cb2d2cde32e82687914
Matan Guttman <matang@sweetinn.com>
2017-07-15 17:06:01 +0300

    stories header complete

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import moment from 'moment';
 import Header from './src/components/Header';
 
 // Create a component
 const App = () => (
-    <Header />
+    <Header titleText={'TOP NEWS STORIES'} subtitleText={moment().format('MMMM Do, YYYY')} />
 );
 
 // Render it to the device
