978e229209b8520c18d970c35d5a14804213052d
Matan Guttman <matang@sweetinn.com>
2017-07-15 15:52:01 +0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+//create comonent
+const Header = () => {
+    return <Text>Albums!</Text>;
+};
+
+//export to other areas of the app
+
+export default Header;
