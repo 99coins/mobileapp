0814682eb6759c669419955a18d5a9e9f1955902
Matan Guttman <matan.guttman@gmail.com>
2017-11-08 23:07:23 +0200

    fixed back problem issue v.1.0.5

--- src/components/Screens/WebView/NewsWebView.js
+++ src/components/Screens/WebView/NewsWebView.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import { WebView, ActivityIndicator } from 'react-native';
+import { WebView, ActivityIndicator, BackHandler } from 'react-native';
+import { Actions } from 'react-native-router-flux'
 
 class NewsWebView extends Component {
 
+componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => this.backAndroid()); // Listen for the hardware back button on Android to be pressed
+}
+
+componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', () => this.backAndroid()); // Remove listener
+ }
+backAndroid() {
+    Actions.pop(); // Return to previous screen
+    return true;// Needed so BackHandler knows that you are overriding the default action and that it should not close the app
+}
 renderLoadingView() {
   return (
     <ActivityIndicator
@@ -11,6 +23,7 @@ renderLoadingView() {
     />
   );
 }
+
 
   render() {
     console.log(this);
