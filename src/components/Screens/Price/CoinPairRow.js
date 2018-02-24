64aa61b1f8d6a9233b2af5eb96907389b27a3809
Matan Guttman <matan.guttman@gmail.com>
2018-02-25 00:17:29 +0200

    cache icons

--- src/components/Screens/Price/CoinPairRow.js
+++ src/components/Screens/Price/CoinPairRow.js
@@ -3,20 +3,21 @@ import { Text, View, StyleSheet } from 'react-native';
 import Colors from '@assets/colors.js';
 import * as Progress from 'react-native-progress';
 import Image from 'react-native-image-progress';
+import { CachedImage } from 'react-native-img-cache';
+
 
 const CoinPairRow = ({ symbol, coinName, priceUsd, percentChange24h, imageUrl }) => {
     return (
         <View style={styles.container}>
             <View style={styles.coinContainer}>
                 <View style={styles.circleContainer}>
-                    <Image 
+                    <CachedImage 
                         style={styles.image} 
                         source={{ uri: imageUrl }} 
-                        cache='force-cache'
+                        //cache='force-cache'
                         indicator={Progress.CircleSnail}
                         indicatorProps={{ color: Colors.themeRed }}
                         resizeMode='contain'
-
                     />
                     <View style={{ position: 'absolute', height: 56, width: 56, borderWidth: 10, borderRadius: 28, borderColor: Colors.gray50 }}></View>
 
