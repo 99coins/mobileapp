0d1eafbc353f70da0b86826c3618fdce411a99eb
Matan Guttman <matan.guttman@gmail.com>
2018-06-02 17:05:30 +0300

    fixed archiving issue (-lreact)

--- android/app/src/main/java/com/nncoins/MainApplication.java
+++ android/app/src/main/java/com/nncoins/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.smooch.rnsmooch.ReactNativeSmoochPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNFirebasePackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
