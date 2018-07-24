014483bd454fdd65712395b17e4b88db717968ee
Matan Guttman <matan.guttman@gmail.com>
2018-07-24 18:35:44 +0300

    lesson row updates

--- src/components/Screens/Lessons/LessonRow.js
+++ src/components/Screens/Lessons/LessonRow.js
@@ -3,42 +3,17 @@ import { Text, View, StyleSheet, TouchableHighlight, Dimensions } from 'react-na
 import Images from '@assets/images.js';
 import Colors from '@assets/colors.js';
 import moment from 'moment';
-import { capitalizeFirstLetter } from '../../common';
 import * as Progress from 'react-native-progress';
 import Image from 'react-native-image-progress';
 
 class LessonRow extends Component {
 
-  // shouldComponentUpdate(nextProps) {
-  //   const update = (this.props.id !== nextProps.id || this.props.selected !== nextProps.selected || this.props.item.contentDetails.duration !== nextProps.item.contentDetails.duration);
-  //   return update;
-  // }
-
-  stripHtmlTags = (str) => {
-    if ((str === null) || (str === '')) {
-      return false;
-    }
-    const strip = str.toString();
-    return strip.replace(/<[^>]*>/g, '');
-  }
-
-  isFromLast24Hours = (date) => {
-    const timeStamp = Math.round(new Date().getTime() / 1000);
-    const timeStampYesterday = timeStamp - (24 * 3600);
-    const is24 = date >= new Date(timeStampYesterday).getTime();
-    return is24;
-  }
-  isToday = (date) => {
-    const today = new Date();
-
-    return today.toDateString() === date.toDateString();
-  }
-  _onPress = () => {
+  onPress = () => {
     this.props.onPressItem(this.props.id);
   };
   render() {
     return (
-      <TouchableHighlight onPress={this._onPress}>
+      <TouchableHighlight onPress={this.onPress}>
         <View style={[styles.container, this.props.selected && styles.selected]}>
           <Image
             style={styles.image}
@@ -48,15 +23,10 @@ class LessonRow extends Component {
             indicatorProps={{
               color: Colors.themeRed,
             }}
-            //resizeMode='contain'
           />
           <View style={styles.textContainer}>
-            <View style={styles.firstLine}>
-              <Text numberOfLines={1} style={styles.titleStyle}>{this.props.item.snippet.title}</Text>
-            </View>
-            <View style={styles.secondLine}>
+              <Text numberOfLines={2} style={styles.titleStyle}>{this.props.item.snippet.title}</Text>
               <Text style={styles.sourceStyle}>{moment.duration(this.props.item.contentDetails.duration).humanize()}</Text>
-            </View>
           </View>
         </View>
       </TouchableHighlight>
@@ -66,18 +36,12 @@ class LessonRow extends Component {
 
 //styling
 const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
     height: 88,
-    width: windowWidth,
-    flex: 1,
     flexDirection: 'row',
-    paddingLeft: 16,
-    paddingTop: 16,
-    paddingBottom: 16,
-    paddingRight: 20,
-    backgroundColor: Colors.gray50
+    padding: 16,
+    backgroundColor: Colors.gray50,
   },
   selected: {
     backgroundColor: Colors.gray100
@@ -91,30 +55,16 @@ const styles = StyleSheet.create({
     overflow: 'hidden'
   },
   textContainer: {
+    justifyContent: 'space-around',
     flex: 1,
-    justifyContent: 'flex-end',
-    paddingLeft: 10
-  },
-  firstLine: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  secondLine: {
-    flex: 0.4,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-    paddingBottom: 5,
-
+    left: 16,
+    marginRight: 16
   },
   titleStyle: {
     flexWrap: 'wrap',
     fontSize: 14,
     fontWeight: 'bold',
-    color: Colors.gray900,
-    lineHeight: 24,
-    paddingBottom: 16,
-
+    color: Colors.gray900
   },
   dateStyle: {
     fontSize: 12,
@@ -123,18 +73,6 @@ const styles = StyleSheet.create({
   sourceStyle: {
     fontSize: 12,
     color: Colors.gray700,
-  },
-  sponsoredSource: {
-    backgroundColor: Colors.sponsoredYellow,
-    top: 4,
-    paddingLeft: 8,
-    paddingRight: 8,
-    paddingTop: 4,
-    paddingBottom: 4,
-    borderRadius: 4,
-    shadowColor: 'black',
-    shadowOpacity: 0.2,
-    shadowRadius: 16,  
   },
   imageStyle: {
     flex: 0.08,
