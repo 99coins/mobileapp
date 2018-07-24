eba2e2b94049af3018e57906c2de2361e4714f71
Matan Guttman <matan.guttman@gmail.com>
2018-07-24 17:25:23 +0300

    removed react-native-youtube

--- ios/Pods/Target Support Files/Pods-NNCoins/Pods-NNCoins-resources.sh
+++ ios/Pods/Target Support Files/Pods-NNCoins/Pods-NNCoins-resources.sh
@@ -91,15 +91,6 @@ EOM
       ;;
   esac
 }
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/../../node_modules/react-native-youtube/assets/YTPlayerView-iframe-player.html"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/../../node_modules/react-native-youtube/assets/YTPlayerView-iframe-player.html"
-fi
-if [[ "$CONFIGURATION" == "Ad-hoc" ]]; then
-  install_resource "${PODS_ROOT}/../../node_modules/react-native-youtube/assets/YTPlayerView-iframe-player.html"
-fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
