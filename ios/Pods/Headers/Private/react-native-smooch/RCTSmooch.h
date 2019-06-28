e038df30ae49362583598c7bcadcc378589f1a72
Matan Guttman <matan.guttman@gmail.com>
2019-06-28 18:08:00 +0300

    upgraded firebase & rnrf, fixed overlay issue

---
+++ ios/Pods/Headers/Private/react-native-smooch/RCTSmooch.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-smooch/ios/RCTSmooch/RCTSmooch.h
---
+++ ios/Pods/Target Support Files/react-native-smooch/react-native-smooch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_react_native_smooch : NSObject
+@end
+@implementation PodsDummy_react_native_smooch
+@end
